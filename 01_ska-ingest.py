ebe534d52c2529aff76224f45904c93e8730d5e0
Kevin Murray <foss@kdmurray.id.au>
2021-05-01 09:24:32 +1000

    ska ingest update

--- 01_ska-ingest.py
+++ 01_ska-ingest.py
@@ -20,9 +20,9 @@ import sys
 # per-genotype depth, AD is per allle read depth, PL is phread-scaled
 # likelihood.
 
-fields=['variants/CHROM', 'variants/POS', 'variants/REF', 'variants/ALT',
-        'variants/DP', 'variants/AD', 'variants/QUAL', 'calldata/GT',
-        'calldata/DP', 'calldata/PL', 'calldata/AD']
+fields=['samples', 'variants/CHROM', 'variants/POS', 'variants/REF',
+        'variants/ALT', 'variants/AD', 'variants/QUAL', 'calldata/GT',
+        'calldata/AD']
 
 
 # Now convert the vcf to a zarr array. We save each reference's calls in their
@@ -37,9 +37,32 @@ allel.vcf_to_zarr(
     group="egrandis",
     compressor=zarr.Blosc(cname="zstd", clevel=5, shuffle=1),
     fields=fields,
-    chunk_width=100,
-    chunk_length=10000,
+    chunk_width=1000,
+    chunk_length=100000,
     log=sys.stderr,
 )
 
+
+allel.vcf_to_zarr(
+    "data/variants-acanthophis/enhanced_filter/mpileup~bwa~Emelliodora_GCA_004368105.2~enhanced_filter.vcf.gz",
+    "data/variants-acanthophis/mpileup~bwa~HBDecra.zarr",
+    group="emelliodora",
+    compressor=zarr.Blosc(cname="zstd", clevel=5, shuffle=1),
+    fields=fields,
+    chunk_width=1000,
+    chunk_length=100000,
+    log=sys.stderr,
+)
+
+
+allel.vcf_to_zarr(
+    "data/variants-acanthophis/enhanced_filter/mpileup~bwa~Esideroxylon_GCA_014182405.1~enhanced_filter.vcf.gz",
+    "data/variants-acanthophis/mpileup~bwa~HBDecra.zarr",
+    group="esideroxylon",
+    compressor=zarr.Blosc(cname="zstd", clevel=5, shuffle=1),
+    fields=fields,
+    chunk_width=1000,
+    chunk_length=100000,
+    log=sys.stderr,
+)
 
